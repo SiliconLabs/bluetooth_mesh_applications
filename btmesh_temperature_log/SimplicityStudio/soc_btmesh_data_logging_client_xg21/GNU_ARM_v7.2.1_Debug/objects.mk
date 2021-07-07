5ed6e2b5c4dad5280c2e0eadd5f069bd9e872cd8
Silabs-ThanhNgo1 <thanh.ngo1@silabs.com>
2021-07-07 13:18:24 +0700

    removed SS5 project folders
    removed SS5 project folders

--- btmesh_temperature_log/SimplicityStudio/soc_btmesh_data_logging_client_xg21/GNU_ARM_v7.2.1_Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := ${USER_SS_SDK_PATH}//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a ${USER_SS_SDK_PATH}//protocol/bluetooth/lib/EFR32MG21/GCC/libbluetooth.a ${USER_SS_SDK_PATH}//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg21_gcc_release.a ${USER_SS_SDK_PATH}//protocol/bluetooth/lib/EFR32MG21/GCC/binapploader.o ${USER_SS_SDK_PATH}//protocol/bluetooth/lib/EFR32MG21/GCC/libbluetooth_mesh.a
-
-LIBS := -lgcc -lc -lm -lnosys
-
